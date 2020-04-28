29e8a5939fd8e869c43d6c40cab7a4300c902c94
argens1203 <argens1203@gmail.com>
2020-04-28 22:11:59 +0800

    First Commit.

---
+++ export.js
@@ -0,0 +1,56 @@
+export function arrayDiff(_oldArr, _newArr) {
+    const oldArr = _oldArr || [];
+    const newArr = _newArr || [];
+    const countDict = {};
+    newArr.forEach(item => {
+        if(item in countDict) {
+            countDict[item] += 1;
+        } else {
+            countDict[item] = 1;
+        }
+    });
+    oldArr.forEach(item => {
+        if(item in countDict) {
+            countDict[item] -= 1;
+        } else {
+            countDict[item] = -1;
+        }
+    });
+    const add = [];
+    const remove = [];
+    for(const key in countDict) {
+        if(countDict[key] > 0) {
+            add.push(key);
+        }
+        if(countDict[key] < 0) {
+            remove.push(key);
+        }
+    }
+
+    return [add, remove];
+}
+
+export function removeItem (arr, item){
+    const idx = arr.indexOf (item);
+    if (idx === -1){
+        return arr;
+    }
+    let arrCopy = [].concat(arr);
+    arrCopy.splice (idx, 1);
+    return arrCopy;
+}
+
+export function addItem (arr, item){
+    if (Array.isArray(arr)){
+        return [].concat(arr, [item]);
+    } else {
+        return [item];
+    }
+}
+
+export function ensureArray (arr){
+    if (Array.isArray(arr)){
+        return arr;
+    }
+    return [];
+}
