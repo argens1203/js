dc9383e187a64f3effdeeaa311de750ecf8498fd
Argens <ng.argens@gmail.com>
2022-07-19 00:16:16 +0800

    init: everything compiles

---
+++ packages/webpage/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ExampleComponent} from 'ui-components';
+import { getString} from 'common-utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ExampleComponent 
+          text={getString('bla')}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
